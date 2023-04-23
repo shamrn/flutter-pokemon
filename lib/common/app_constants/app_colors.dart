14f3186dfb5e2c5c01fa41809a4a4a3735a2583b
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-23 18:33:44 +0300

    Updated 'AppColors'

--- lib/common/app_constants/app_colors.dart
+++ lib/common/app_constants/app_colors.dart
@@ -6,4 +6,6 @@ class AppColors {
   static const Color canvasColor = Color(0xFF1C1D1F);
   static const Color primaryColor = Color(0xFF1C1D1F);
   static const Color secondColor = Color(0xFFE16441);
+  static const Color darkenedBackgroundColor = Color(0xFF6D6463);
+  static const Color primaryTextColor = Color(0xFFE6D9D9);
 }
