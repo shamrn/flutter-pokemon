37dd073bd9111c8895458362e28dae6bcb96f336
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-23 23:09:20 +0300

    For pokemon models added 'id' field

--- lib/feature/data/datasource/pokeapi_client/models.dart
+++ lib/feature/data/datasource/pokeapi_client/models.dart
@@ -1,13 +1,15 @@
 // The file implements DTO models
 
 class PokemonDTO {
+  final int id;
   final String name;
   final String imageUrl;
 
-  PokemonDTO({required this.name, required this.imageUrl});
+  PokemonDTO({required this.id, required this.name, required this.imageUrl});
 
   PokemonDTO.fromJson(Map<dynamic, dynamic> json)
-      : name = json['name'],
+      : id = json['id'],
+        name = json['name'],
         imageUrl =
             json['sprites']['other']['official-artwork']['front_default'];
 }
--- lib/feature/data/repositories/pokemon_repository.dart
+++ lib/feature/data/repositories/pokemon_repository.dart
@@ -2,16 +2,18 @@ import 'package:flutter_pokemon/feature/data/datasource/pokeapi_client/models.da
 import 'package:flutter_pokemon/feature/data/datasource/pokeapi_client/services.dart';
 import 'package:flutter_pokemon/feature/data/models/pokemon_models.dart';
 
-class PokemonDetailRepository {
+class SearchPokemonRepository {
   Future<PokemonModel> getPokemon({required String name}) async {
     PokemonDTO pokemon = await getPokemonByName(name: name);
-    return PokemonModel(name: pokemon.name, imageUrl: pokemon.imageUrl);
+    return PokemonModel(
+        id: pokemon.id, name: pokemon.name, imageUrl: pokemon.imageUrl);
   }
 }
 
 class RandomPokemonRepository {
   Future<PokemonModel> getPokemon({required int numberPokemon}) async {
     PokemonDTO pokemon = await getRandomPokemon(numberPokemon: numberPokemon);
-    return PokemonModel(name: pokemon.name, imageUrl: pokemon.imageUrl);
+    return PokemonModel(
+        id: pokemon.id, name: pokemon.name, imageUrl: pokemon.imageUrl);
   }
 }
