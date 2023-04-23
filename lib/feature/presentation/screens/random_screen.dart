00927aaf1ea533e322b46d0e196ec78f78244925
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-23 18:35:01 +0300

    Updated 'random_screen' and 'search_screen' added new widgets

--- lib/feature/presentation/screens/random_screen.dart
+++ lib/feature/presentation/screens/random_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_pokemon/common/widgets/primary_widget.dart';
+import 'package:flutter_pokemon/feature/presentation/widgets/card_widget.dart';
 
 class RandomScreen extends StatelessWidget {
   const RandomScreen({Key? key}) : super(key: key);
@@ -9,7 +10,11 @@ class RandomScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return PrimaryWidget(
       screenTitle: AppLocalizations.of(context)!.nameRandomScreen,
-      child: Text('Random screen'),
+      child: const CardWidget(
+        name: 'pikachu',
+        imageUrl:
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+      ),
     );
   }
 }
