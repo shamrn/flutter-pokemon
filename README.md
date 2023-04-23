ef420a443f2e50a7e9ad71d6e28a77edc1bba071
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-23 10:56:12 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Flutter pokemon application
+
+---
+
+Mobile cross-platform application for "Flutter pokemon application" project based
+on [Flutter framework](https://flutter.dev/).
+
+## Summary
+
+Sources:
+- Mobile repository — [Github]()
+- Backend API — [Open pokemon API](https://pokeapi.co/)
+
+## Description
+...
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PokemonApp());
+}
+
+class PokemonApp extends StatelessWidget {
+  const PokemonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
