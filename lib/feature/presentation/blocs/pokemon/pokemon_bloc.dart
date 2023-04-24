9ec974c483d5f3f82387df53a24547c28c9e2d2f
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-24 14:34:09 +0300

    In 'PokemonBloc' added 'AppLocalizations', for set error message

--- lib/feature/presentation/blocs/pokemon/pokemon_bloc.dart
+++ lib/feature/presentation/blocs/pokemon/pokemon_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_pokemon/common/app_constants/app_settings.dart';
 import 'package:flutter_pokemon/feature/data/datasource/pokeapi_client/exceptions.dart';
 import 'package:flutter_pokemon/feature/data/models/pokemon_models.dart';
 import 'package:flutter_pokemon/feature/data/repositories/pokemon_repository.dart';
@@ -6,6 +8,9 @@ import 'package:flutter_pokemon/feature/presentation/blocs/pokemon/pokemon_event
 import 'package:flutter_pokemon/feature/presentation/blocs/pokemon/pokemon_state.dart';
 
 class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
+  final Future<AppLocalizations> _appLocalizations =
+      AppLocalizations.delegate.load(AppSettings.defaultLocale);
+
   PokemonBloc() : super(PokemonInitialState()) {
     on<SearchPokemonLoadEvent>(_onSearchPokemon);
     on<RandomPokemonLoadEvent>(_onRandomPokemon);
@@ -22,16 +27,17 @@ class PokemonBloc extends Bloc<PokemonEvent, PokemonState> {
   }
 
   void _loadPokemon(PokemonRepository pokemonRepository) async {
+    AppLocalizations locale = await _appLocalizations;
+
     emit(PokemonLoadingState());
     try {
       PokemonModel pokemon = await pokemonRepository.getPokemon();
       emit(PokemonLoadedState(pokemon: pokemon));
     } on PokeAPINotFoundException {
-      emit(PokemonErrorState(
-          errorMessage: 'Покемон не найден')); // todo move in locale
+      emit(PokemonErrorState(errorMessage: locale.pokemonNotFound));
     } catch (_) {
       emit(PokemonErrorState(
-          errorMessage: 'Ошибка получения данных')); // todo move in locale
+          errorMessage: locale.pokemonRetrievalError));
     }
   }
 }
